72791b4d1b9ba49079e38e853afc2c5a5cd0b3f0
Moqri <moqri@ufl.edu>
2017-04-15 09:47:57 -0400

    .

--- crossref.py
+++ crossref.py
@@ -1,18 +1,18 @@
 import os
-import wget
-crossref='http://api.crossref.org/journals/<issn>/works?filter=from-pub-date:<year>,until-pub-date:<year>'
-for issn in ['1047-7047','0742-1222'][1:2]:
+import urllib
+crossref='http://api.crossref.org/journals/<issn>/works?rows=1000&filter=from-pub-date:<year>,until-pub-date:<year>'
+for issn in ['1047-7047','0742-1222']:
 	print issn	
 	journal_folder='doi/'+issn+'/'
 	if not os.path.exists(journal_folder):
 		os.makedirs(journal_folder)
 	crossref_issn=crossref.replace('<issn>',issn)
-	for year in range(2000,2018):
+	for year in range(1990,2018):
 		print year,': ' 
 		url=crossref_issn.replace('<year>',str(year))
 		print url
 		try: 
-			filename = wget.download(url,journal_folder+str(year))
+			urllib.urlretrieve (url,journal_folder+str(year))
 		except:
 			pass
 		print
